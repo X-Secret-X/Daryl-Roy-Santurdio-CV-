32e2062873e9ed7a39490c817a0875b350da07d9
Daryl Roy Santurdio <138319100+X-Secret-X@users.noreply.github.com>
2024-12-04 19:40:47 -0800

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -79,20 +79,7 @@ async function fetchData(type = "skills") {
     return data;
 }
 
-function showSkills(skills) {
-    let skillsContainer = document.getElementById("skillsContainer");
-    let skillHTML = "";
-    skills.forEach(skill => {
-        skillHTML += `
-        <div class="bar">
-              <div class="info">
-                <img src=${skill.icon} alt="skill" />
-                <span>${skill.name}</span>
-              </div>
-            </div>`
-    });
-    skillsContainer.innerHTML = skillHTML;
-}
+
 
 
 
